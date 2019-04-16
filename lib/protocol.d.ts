47fb8e2ca983f9ae97a4a80791eb765621043a01
Сухарик <suhr@i2pmail.org>
2019-04-16 19:27:06 +0300

    Use interfaces and namespaces

--- lib/protocol.d.ts
+++ lib/protocol.d.ts
@@ -1,41 +1,122 @@
-type ServerMessage = ToxResponse | ToxEvent;
+export type ServerMessage = ToxResponse | ToxEvent;
 
-type ToxRequest =
-    { "request": "Info" } |
-    { "request": "AddFriend", "tox_id": string, "message": string} |
-    { "request": "AddFriendNorequest", "tox_id": string } |
-    {
+export type ToxRequest =
+    Requests.Info |
+    Requests.AddFriend |
+    Requests.AddFriendNorequest |
+    Requests.SendFriendMessage;
+
+export type ToxResponse =
+    Responses.Ok |
+    Responses.Info |
+    Responses.AddFriendError |
+    Responses.SendFriendMessage;
+
+export type ToxEvent =
+    Events.ConnectionStatus |
+    Events.FriendRequest |
+    Events.FriendMessage |
+    Events.FriendConnectionStatus;
+
+export type ConnectionStatus = "None" | "Tcp" | "Udp";
+export type MessageType = "Normal" | "Action";
+
+export namespace Requests {
+    export interface Request {
+        "request": string,
+    }
+
+    export interface Info extends Request {
+        "request": "Info",
+    }
+
+    export interface AddFriend extends Request {
+        "request": "AddFriend",
+        "tox_id": string,
+        "message": string
+    }
+
+    export interface AddFriendNorequest extends Request {
+        "request": "AddFriendNorequest",
+        "tox_id": string
+    }
+
+    export interface SendFriendMessage extends Request {
         "request": "SendFriendMessage",
         "friend": number,
         "kind": MessageType,
         "message": string
-    };
+    }
+}
 
-type ToxResponse =
-    { "response": "Ok" } |
-    { "response": "Info", "name": string, "tox_id": string } |
-    { "response": "AddFriendError", "error": AddFriendError } |
-    { "response": "SendFriendMessageError", "error": SendFriendMessageError };
+export namespace Responses {
+    export interface Response {
+        "response": string,
+    }
 
-type ToxEvent =
-    { "event": "ConnectionStatus", "status": ConnectionStatus } |
-    { "event": "FriendRequest", "public_key": number[], "message": string} |
-    { "event": "FriendMessage", "friend": number, "kind": MessageType, "message": string } |
-    { "event": "FriendConnectionStatus", "friend": number, "status": ConnectionStatus};
+    export interface Ok extends Response {
+        "response": "Ok"
+    }
 
-type ConnectionStatus = "None" | "Tcp" | "Udp";
-type MessageType = "Normal" | "Action";
+    export interface Info extends Response {
+        "response": "Info",
+        "name": string,
+        "tox_id": string
+    }
 
-type AddFriendError = 
-    "TooLong" |
-    "NoMessage" |
-    "OwnKey" |
-    "AlreadySent" |
-    "BadChecksum" |
-    "SetNewNospam";
+    export interface AddFriendError extends Response {
+        "response": "AddFriendError",
+        "error": Errors.AddFriendError
+    }
 
-type SendFriendMessageError = 
-    "NotFound" |
-    "NotConnected" |
-    "TooLong" |
-    "Empty";
+    export interface SendFriendMessage extends Response {
+        "response": "SendFriendMessageError",
+        "error": Errors.SendFriendMessageError
+    }
+}
+
+export namespace Events {
+    export interface Event {
+        event: string
+    }
+
+    export interface ConnectionStatus extends Event {
+        "event": "ConnectionStatus",
+        "status": ConnectionStatus
+    }
+
+    export interface FriendRequest extends Event {
+        "event": "FriendRequest",
+        "public_key": number[],
+        "message": string
+    }
+
+    export interface FriendMessage extends Event {
+        "event": "FriendMessage",
+        "friend": number,
+        "kind": MessageType,
+        "message": string
+    }
+
+    export interface FriendConnectionStatus extends Event {
+        "event": "FriendConnectionStatus",
+        "friend": number,
+        "status": ConnectionStatus
+    }
+}
+
+export namespace Errors {
+    export type AddFriendError =
+        "TooLong" |
+        "NoMessage" |
+        "OwnKey" |
+        "AlreadySent" |
+        "BadChecksum" |
+        "SetNewNospam";
+
+    export type SendFriendMessageError =
+        "NotFound" |
+        "NotConnected" |
+        "TooLong" |
+        "Empty";
+}
